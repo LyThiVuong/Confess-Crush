bd6dcd69e5f934abbc233a1aa0ccad4f2383071e
LyThiVuong <97218967+LyThiVuong@users.noreply.github.com>
2022-01-15 10:03:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "LTV",
+    introTitle: 'Gửi con người độc ác=)',
+    introDesc: `BẠN BỊ MÙ À ? `,
+    btnIntro: 'SAO TỰ NHIÊN HỎI TỚ MÙ?',
+    title: 'BỞI VÌ CẬU KHÔNG THẤY TÌNH CẢM CỦA TỚ=)',
+    desc: '所以你有病了'
+    btnYes: 'ĐÚNG ZÙI HUHU',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'LÀM NY TỚ NHA',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tối nay 7h,XEM PHIM MA nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/ly.ha.39982631/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
